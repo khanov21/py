1a9503388890b274987db656a10366768875d084
mirzaali.dev <khanov_21@icloud.com>
2026-06-08 14:30:56 +0500

    3-masala

---
+++ 3masala.py
@@ -0,0 +1,12 @@
+#5 - masala
+# def string_s(c, s1, s2):
+#     return s1.replace(c, s2 + c)
+# print(string_s("a", "banana", "lar"))
+
+# #6 - masala
+# def string_m(s1, s2):
+#     if s2 in s1:
+#         return True
+#     else:
+#         return False
+# print(string_m("banana", "nan"))
