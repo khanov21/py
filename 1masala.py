4bdc2ae44533d5402031a22cd87d5e369353c65a
mirzaali.dev <khanov_21@icloud.com>
2026-06-08 14:29:59 +0500

    1-masala

---
+++ 1masala.py
@@ -0,0 +1,24 @@
+# #1 - masala
+# n = int(input("Son kiriting: "))
+# m = input("satir kiriting: ")
+# uzunlik = len(m)
+# if n > uzunlik:
+#     farq = n - uzunlik
+#     print(m[farq:])
+# elif n < uzunlik:
+#     yetmagan_joy = uzunlik - n
+#     natija = "*" * yetmagan_joy + m
+#     print(natija)
+# else:
+#     natija = m
+# print(natija)
+
+# #2 - masala
+# n1 = int(input("Son kiriting: "))
+# n2 = int(input("Son kiriting: "))
+# s1 = input("satir kiriting: ")
+# s2 = input("satir kiriting: ")
+# bolak1 = s1[:n1]
+# bolak2 = s2[:n2]
+# natija = bolak1 + bolak2
+# print(natija)   
