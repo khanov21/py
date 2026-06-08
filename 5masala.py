0ed182a14d740ba979f18ed867e0e064d77daf88
mirzaali.dev <khanov_21@icloud.com>
2026-06-08 14:31:44 +0500

    5-masala

---
+++ 5masala.py
@@ -0,0 +1,14 @@
+# #9 - masala
+# def string_k(s1, s2):
+#     index = s1.find(s2)
+#     if index != -1:
+#         return s1
+#     uzunlik = len(s1)
+#     yangisi = s1[:index] + s2 + s1[index:]
+#     return yangisi
+# print(string_k("banana", "an")) 
+
+#10 - masala
+def string36(S1, S2, S3):
+    return S1.replace(S2, S3, 1)
+print(string36("banana", "an", "xy"))
