c13063987837717790c63427db320b9a6cabb19e
mirzaali.dev <khanov_21@icloud.com>
2026-06-08 14:30:32 +0500

    2-masala

---
+++ 2masala.py
@@ -0,0 +1,9 @@
+#3 - MASALA
+# def string(c, s):
+#     return s.replace(c, c*2)
+# print(string("a", "banana"))
+
+# #4 - MASALA
+# def string_s(c, s1, s2):
+#     return s1.replace(c, s2 + c)
+# print(string_s("a", "banana", "b"))
