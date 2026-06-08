c0e23861afe1236afae76873ffc8a18b541cafd3
mirzaali.dev <khanov_21@icloud.com>
2026-06-08 14:31:21 +0500

    4-masala

---
+++ 4masala.py
@@ -0,0 +1,9 @@
+#7 - masala
+# def string_d(s1, s2):
+#     return s1.count(s2)
+# print(string_d("banana", "an"))
+
+# #8 - masala
+# def string_z(s1, s2):
+#     return s1.replace(s2, "", 1)
+# print(string_z("banana", "an"))
